a98e23e8b370542b58bcf51fdb724a4c3aff29e4
Xwilarg <xwilarg@protonmail.com>
2022-03-26 18:13:30 +0100

    Add map & player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace OneHourJam362
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField]
+        private float _speed;
+
+        private Vector2 _dir;
+        private Rigidbody2D _rb;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            _dir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized * _speed;
+        }
+
+        private void FixedUpdate()
+        {
+            _rb.velocity = _dir;
+        }
+    }
+
+}
